3f7e6068e61d1078ecd974377ac04179857afc1d
Tatsuya Kawasaki <ta2ya.xxx@gmail.com>
2016-03-13 23:48:29 +0900

    MermaidJSの安定度を上げるように修正

--- app/sources/js/components/helpers/mermaid.js
+++ app/sources/js/components/helpers/mermaid.js
@@ -6,22 +6,45 @@ class Mermaid extends React.Component {
 
   constructor(props) {
     super(props)
-    this.state = {diagram: "..."}
+    this.state = {
+      diagram: "生成中 ..."
+    }
   }
 
   componentWillReceiveProps(nextProps) {
-    mermaidAPI.render("mermaid", nextProps.text, (html) => this.setState({diagram: html}))
+    mermaidAPI.render(this.getId(), nextProps.text, (html) => {
+      this.setState({diagram: html})
+    })
   }
 
-  componentDidMount () {
-    mermaidAPI.render("mermaid", this.props.text, (html) => this.setState({diagram: html}))
+  componentDidMount() {
+    mermaidAPI.render(this.getId(), this.props.text, (html) => {
+      this.setState({diagram: html})
+    })
+  }
+
+  componentDidUpdate() {
+    mermaidAPI.render(this.getId(), this.props.text, (html) => {
+      this.setState({diagram: html})
+    })
+  }
+
+  getId() {
+    let uuid = "", i, random
+    for (i = 0; i < 32; i++) {
+      random = Math.random() * 16 | 0
+
+      if (i == 8 || i == 12 || i == 16 || i == 20) {
+        uuid += "-"
+      }
+      uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16)
+    }
+    return uuid
   }
 
   render() {
     return (
-      (this.state.diagram) ? 
-      <div className="mermaid" dangerouslySetInnerHTML={{__html: this.state.diagram}}></div> : 
-      <div></div>
+      <div className="mermaid" dangerouslySetInnerHTML={{__html: this.state.diagram}}></div>
     )
 
   }
